991144bc8e4ae82586a3c06be3c282205c97e2d8
Christian Lustre <clustrep@gmail.com>
2022-12-07 12:29:58 -0500

    primer guardado

---
+++ CNTI365.FACTUR/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CNTI365.FACTUR.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+      
+    }
+}
